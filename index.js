23a471bbac80f4b0e17dfe4e8944d0b0b2ab94f7
Sachidananda S V <sachivips@gmail.com>
2024-01-23 10:11:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("Googledotcom");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
